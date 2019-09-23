c52adf688e3b61e9c7adf74ee01b262946fafda7
jakegalligan <galligan1996@gmail.com>
2019-09-23 15:18:03 -0400

    Set up react router within application

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {App} from './App';
+import {LandingPage} from './components/LandingPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path = '/' component={LandingPage} />
+            </Switch>
+        </App>
+    </Router>, 
+document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
