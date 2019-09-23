c33840dc4511fcf1220b6c0f7fcbf35c0ac7517c
jakegalligan <galligan1996@gmail.com>
2019-09-23 17:57:18 -0400

    Allow for the typing text to occur on the landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Typing from 'react-typing-animation';
+import {Col, Row, Container} from 'react-bootstrap';
+import Paper from '@material-ui/core/Paper';
 
 export const LandingPage = () => {
     return (
         <div>
-            What up
+            <Container>
+                <Row>
+                <Typing speed={150}>
+                    <span id="longtitle-landingpage">blackgra/eywhite</span>
+                        <Typing.Backspace count={15} />
+                        <span>/g/w</span>
+                </Typing>
+                </Row>
+                <Row>
+                    <Col>
+                        yeah
+                    </Col>
+                    <Col>
+                        hah
+                    </Col>
+                    <Col>
+                        smell cologne
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
